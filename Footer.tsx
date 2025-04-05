8643bb3900f6ea6611b58021bbef1beb8dd61697
anacarolinecastrog <anacarolinecgomes@gmail.com>
2025-04-04 21:08:08 -0300

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="w-full text-center text-xs text-gray-400 py-4">
+      {/* 
+        A linha abaixo exibia a marca do Typebot. Ela foi comentada:
+        <a href="https://typebot.io" target="_blank" rel="noopener noreferrer">
+          Powered by Typebot
+        </a> 
+      */}
+
+      {/* Se quiser, pode colocar seu nome ou marca aqui: */}
+      {/* <span>Feito com ❤️ por MinhaMarca</span> */}
+    </footer>
+  );
+};
+
+export default Footer;
